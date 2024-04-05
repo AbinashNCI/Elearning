04512d8e91e32986983c7bea41f28787c47dc807
EC2 Default User <ec2-user@ip-172-31-11-51.us-west-2.compute.internal>
2024-04-05 12:04:24 +0000

    Deployement

--- elearningApp/settings.py
+++ elearningApp/settings.py
@@ -116,7 +116,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
@@ -127,3 +126,9 @@ MEDIA_ROOT = BASE_DIR
 
 # URL to access media files
 MEDIA_URL = ''
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
+
+STATIC_URL = '/static/'
+STATIC_ROOT =  'staticfiles'
